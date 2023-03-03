f61056d3df30ec88668618ef405d22749cb43261
khadija-mahdi <khadijamahdi6@gmail.com>
2023-03-03 22:50:11 +0100

    ok

--- exec/implement_builtins/implement_builtins.c
+++ exec/implement_builtins/implement_builtins.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   implement_builtins.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kmahdi <kmahdi@student.1337.ma>            +#+  +:+       +#+        */
+/*   By: khadija-mahdi <khadija-mahdi@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 15:27:03 by kmahdi            #+#    #+#             */
-/*   Updated: 2023/03/03 18:56:29 by kmahdi           ###   ########.fr       */
+/*   Updated: 2023/03/03 22:44:44 by khadija-mah      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../exec.h"
-
 
 void unset_command(m_node *node)
 {
 	char **env;
 	int len;
 	int i;
+	char **va_unset;
 
 	env = get_env(NULL);
 	if (ft_strcmp(node->command ,"unset") == 0)
@@ -33,12 +33,18 @@ void unset_command(m_node *node)
 			while(node->arguments[i])
 			{
 				if(!ft_strncmp(*env ,node->arguments[i], ft_strlen(node->arguments[i])) && (*env)[ft_strlen(node->arguments[i])] == '=')
-				{
-					*env  = *env + 1;
-					env++;
+				{	
+					va_unset = env + 1;
+					while(*va_unset)
+					{
+						*(va_unset - 1)  = *va_unset;
+						va_unset++;
+					}
 				}
 				else
 					i++;
+				*(va_unset - 1) = NULL;
+				
 			}
 			env++;
 		}
@@ -71,11 +77,17 @@ char **get_export(char **env, int len)
 	return (export);
 }
 
+
 void export_command(m_node *node)
 {
 	char **env;
 	int len;
 	char **export;
+	char *name;
+	char *value;
+	int start = 0;
+	char **new_env;
+	int i = 0;
 
 	len = 0;
 	env = get_env(NULL);
@@ -92,6 +104,28 @@ void export_command(m_node *node)
 				export++;
 			}
 		}
+		else
+		{
+			while (node->arguments[1][start] != '=' && node->arguments[1][start])
+				start++;
+			
+			name = ft_substr(node->arguments[1], 0, start);
+			value = ft_substr(node->arguments[1] , start , ft_strlen(node->arguments[1]));
+			// printf("name --> %s value ---> %s\n" ,name , value);
+			if(ft_strcmp(node->arguments[1], "=")== 0)
+			{
+				new_env = malloc((len + 2) * sizeof(char *));
+				while(i < len)
+				{
+					new_env[i] = env[i];
+					i++;
+				}
+				new_env[len] = node->arguments[1];
+    			new_env[len + 1] = NULL;
+				env = new_env;
+				free_list(new_env);
+			}
+		}	
 	}
 
 }
